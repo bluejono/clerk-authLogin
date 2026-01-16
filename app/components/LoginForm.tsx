cf6b636376a217f03b2f7671ad4a517c02ead534
João Nogueira <devjoaoluiz@gmail.com>
2026-01-16 01:10:30 -0300

    update: high fidelity pattern on

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,32 +1,54 @@
+import Image from "next/image";
+
 const LoginForm = () => {
   return (
     <div className="flex h-screen w-screen">
       {/* Div Esquerda */}
       <div className="w-1/2 h-full bg-white text-black p-[40px]">
-        <form className="justify-center gap-5 w-full h-full border border-black flex flex-col items-center ">
-          LOGO
+        <form className="justify-center gap-5 w-full h-full border border-gray-500/25 flex flex-col items-center">
+          Logo
+          <h1 className="text-3xl font-medium">Get started with GameTime</h1>
+          <p className="text-gray-600">
+            Already have an Account? <a className="underline">Log in</a>
+          </p>
           <label className="flex flex-col">
-            Email
+            <h2 className="mb-3">Enter your Email</h2>
+
             <input
               type="email"
-              className="border border-black rounded-lg px-4 py-2 w-lg"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-md"
+              placeholder="name@example.com"
             />
           </label>
           <label className="flex flex-col">
-            Password
+            <h2 className="mb-3">Password</h2>
+
             <input
               type="password"
-              className="border border-black rounded-lg px-4 py-2 w-lg"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-md"
+              placeholder="your password"
             />
           </label>
-          <button className="p-2 border w-1/2 rounded-sm">OPA</button>
+          <button className="p-2 w-md rounded-md bg-black text-gray-200">
+            Continue
+          </button>
           <p>OR</p>
-          <button className="p-2 border w-1/2 rounded-sm">OPA</button>
+          <button className="p-2 flex items-center justify-center gap-3 border border-gray-300 w-md rounded-sm">
+            <Image
+              src="/google.png"
+              alt="google symbol"
+              width={20}
+              height={20}
+            ></Image>
+            Continue with Google
+          </button>
         </form>
       </div>
 
       {/* Div Direita */}
-      <div className="w-1/2 h-full bg-amber-800"></div>
+      <div className="w-1/2 h-full bg-amber-800 relative">
+        <Image src="/newBg2.jpg" alt="Pixels" fill className="object-cover" />
+      </div>
     </div>
   );
 };
